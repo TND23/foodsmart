b8351cb0cd49bfa79e7f357a6a0ab190003d6fce
Tommy <tommyn@isomedia.com>
2014-03-13 12:32:54 -0700

    better fk relations

--- db/migrate/20140313191719_add_foreign_key_to_endorsements.rb
+++ db/migrate/20140313191719_add_foreign_key_to_endorsements.rb
@@ -1,4 +1,7 @@
 class AddForeignKeyToEndorsements < ActiveRecord::Migration
   def change
+  	add_column :endorsements, :recipe_id, :integer
+  	#author
+  	add_column :endorsements, :user_id, :integer 
   end
 end
